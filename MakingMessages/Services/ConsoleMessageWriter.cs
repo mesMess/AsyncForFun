d53bdd86ce760b8af10ef5309a3b14f7a55d1b9d
Michael E Smithzer <michael.smithzer@gmail.com>
2020-12-20 11:04:58 -0500

    Some IoC to get us started down the road of DIP

---
+++ MakingMessages/Services/ConsoleMessageWriter.cs
@@ -0,0 +1,15 @@
+﻿using MakingMessages.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakingMessages.Services
+{
+    public class ConsoleMessageWriter : IMessageWriter
+    {
+        public void Write(string message) =>
+            Console.WriteLine("I'm the console message service...");
+    }
+}
---
+++ MakingMessages/Worker.cs
@@ -0,0 +1,36 @@
+﻿using MakingMessages.Interfaces;
+using MakingMessages.Services;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MakingMessages
+{
+    public class Worker : BackgroundService
+    {
+        //private readonly MessageWriter _messageWriter = new MessageWriter();
+        private readonly IMessageWriter _messageWriter;
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(IMessageWriter messageWriter, ILogger<Worker> logger)
+        {
+            _messageWriter = messageWriter;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _messageWriter.Write($"Working running at: {DateTimeOffset.Now}");
+                _logger.LogInformation($"Working running at: {DateTimeOffset.Now}");
+                await Task.Delay(2000, stoppingToken);
+            }
+        }
+    }
+}
