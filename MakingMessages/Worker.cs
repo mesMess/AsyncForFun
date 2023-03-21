28f4204a016a0910308937dbba25649aee5aa8d6
Michael Smithzer <mesMess@users.noreply.github.com>
2023-03-20 20:46:07 -0400

    Cleaning up some using statements

--- MakingMessages/Worker.cs
+++ MakingMessages/Worker.cs
@@ -3,9 +3,6 @@ using MakingMessages.Services;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,21 +11,21 @@ namespace MakingMessages
     public class Worker : BackgroundService
     {
         //private readonly MessageWriter _messageWriter = new MessageWriter();
-        private readonly IMessageWriter _messageWriter;
-        private readonly ILogger<Worker> _logger;
+        private readonly IMessageWriter messageWriter;
+        private readonly ILogger<Worker> logger;
 
         public Worker(IMessageWriter messageWriter, ILogger<Worker> logger)
         {
-            _messageWriter = messageWriter;
-            _logger = logger;
+            this.messageWriter = messageWriter;
+            this.logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _messageWriter.Write($"Working running at: {DateTimeOffset.Now}");
-                _logger.LogInformation($"Working running at: {DateTimeOffset.Now}");
+                this.messageWriter.Write($"Working running at: {DateTimeOffset.Now}");
+                logger.LogInformation($"Working running at: {DateTimeOffset.Now}");
                 await Task.Delay(2000, stoppingToken);
             }
         }
